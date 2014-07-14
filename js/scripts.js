b9cf8b4eea4a82ac472fe4f92a8dc5f3dbead3a9
Dylan Peerenboom <student@epicodus.com>
2014-07-14 16:51:08 -0700

    last minute notes to take home

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 var checkTriangle = function (side1, side2, side3){
 
+  var triangleSides = (side1, side2, side3)
+
   var equilateral = function(){
     if (side1===side2===side3){
       return 'Equilateral Triangle';
     };
-    else {
+    else if (side1===side2){
       return 'Not an equilateral Triangle';
     };
   };
 };
+//need to assign values and sort three triangleSides
+//if smaller sides greater than largest side not a triangle
+//  then return not a triangle
+//if smaller sides sum less than largest side they do form a triangle
+//check to see what type of triangle it is
