b3e59213316738ce512c0c71479e38a715398e67
Dylan Peerenboom <student@epicodus.com>
2014-07-14 16:45:50 -0700

    added notes, time to wrap up

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("hello");
+var checkTriangle = function (side1, side2, side3){
+
+  var equilateral = function(){
+    if (side1===side2===side3){
+      return 'Equilateral Triangle';
+    };
+    else {
+      return 'Not an equilateral Triangle';
+    };
+  };
+};
--- specs/specs.js
+++ specs/specs.js
@@ -1,1 +1,5 @@
-
+describe('checkTriangle', function(){
+  it('checks whether all the sides are equal', function(){
+    equilateral(1,1,1).should.equal('Equilateral Triangle');
+  })
+})
