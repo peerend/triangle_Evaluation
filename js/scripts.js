4cf4b514dae7601842d9b0212f24e2e939c94159
Dylan Peerenboom <peerend@gmail.com>
2014-07-14 19:57:09 -0700

    fixed the sort bug, added isosceles function and check

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 var checkTriangle = function (side1, side2, side3){
-
   if (notTriangle(side1, side2, side3)){
   return 'is not a triangle';
+  } else if(equilateralTriangle(side1, side2, side3)){
+    return 'is a equilateral triangle';
+  } else if(isoscelesTriangle(side1, side2, side3)){
+    return 'is a isosceles triangle';
   }
-  else if(equilateralTriangle(side1, side2, side3)){
-    return 'is an equilateral triangle';
+  else{
+    return 'is a isosceles triangle';
   }
 }
+
 var notTriangle = function(side1, side2, side3){
   var triangleSides = [side1, side2, side3];
   var sortedSides = triangleSides.sort();
-  if ((sortedSides[1]+sortedSides[2])>sortedSides[3]){
+
+  if ((sortedSides[0]+sortedSides[1])<sortedSides[2]){
     return true;
   }else {
     return false;
@@ -23,17 +28,14 @@ var equilateralTriangle = function(side1, side2, side3){
     return false;
   }
 }
-//need to assign values and sort three triangleSides
-//if smaller sides greater than largest side not a triangle
-//  then return not a triangle
-//if smaller sides sum less than largest side they do form a triangle
-//check to see what type of triangle it is
+var isoscelesTriangle = function(side1, side2, side3){
+  var triangleSides = [side1, side2, side3];
+  var sortedSides = triangleSides.sort();
 
-  // var equilateral = function(){
-  //   if (side1===side2===side3){
-  //     return 'Equilateral Triangle';
-  //   }
-  //   else if (side1===side2){
-  //     return 'Not an equilateral Triangle';
-  //   };
-  // };
+  if (side1 === side2 || side2 === side3){
+    return true;
+  }else {
+    return false;
+  }
+}
+
